72410821dfe873b689003ec6ce79209098b08818
Christian Betz <christian.betz@performance-media.de>
2015-10-19 12:04:42 +0200

    switches to http-kit instead of clj-http for asynchronous calls

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-http "2.0.0"]
+                 [http-kit "2.1.18"]
                  [cheshire "5.5.0"]
                  [gorillalabs/config "1.0.1"]
                  ]
