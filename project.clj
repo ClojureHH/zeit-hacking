720bcc3869e552f60ae6fc574aadd994542b07c1
Christian Betz <christian.betz@performance-media.de>
2015-10-19 11:08:16 +0200

    adds GorillaREPL as a plugin, adds a first (almost blank) worksheet.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/chrisbetz/zeit-hacking"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :plugins [[lein-gorilla "0.3.4"]]
+  )
