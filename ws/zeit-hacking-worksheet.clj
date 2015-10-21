59fec9b326fb6d84dd5aca51ce3255c4a560c484
Christian Betz <christian.betz@performance-media.de>
2015-10-21 12:06:49 +0200

    extends the worksheet to use a simple custom renderer

--- ws/zeit-hacking-worksheet.clj
+++ ws/zeit-hacking-worksheet.clj
@@ -16,6 +16,7 @@
             [org.httpkit.client :as http]
             [gorilla-plot.core :as plot]
             [cheshire.core :as json]
+            [zeit-hacking.renderer :as zh]
             ))
 
 (def conf (config/init))
@@ -30,6 +31,8 @@
               :user-agent "ClojureMeetupHH-Test"
               :headers {"X-Authorization" (:api-key conf)}})
 
+
+(zh/content-view "test")
 
 ;(println (:teaser_text (first (:matches (json/parse-string (:body ...
 
@@ -54,7 +57,7 @@
 ;;; 
 ;; <-
 ;; =>
-;;; {"type":"vega","content":{"width":400,"height":247.2187957763672,"padding":{"top":10,"left":50,"bottom":20,"right":10},"data":[{"name":"5ab09b52-fd81-4aee-820d-c44ef550e087","values":[{"x":"bundesrat","y":1},{"x":"justizbehoerde","y":1},{"x":"corny-littmann","y":1},{"x":"bonn","y":2},{"x":"weizen","y":1},{"x":"klassik","y":1},{"x":"daenemark","y":1},{"x":"frankfurt-am-main","y":1},{"x":"fluechtling","y":4},{"x":"dresden","y":1}]}],"marks":[{"type":"rect","from":{"data":"5ab09b52-fd81-4aee-820d-c44ef550e087"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"width":{"scale":"x","band":true,"offset":-1},"y":{"scale":"y","field":"data.y"},"y2":{"scale":"y","value":0}},"update":{"fill":{"value":"steelblue"},"opacity":{"value":1}},"hover":{"fill":{"value":"#FF29D2"}}}}],"scales":[{"name":"x","type":"ordinal","range":"width","domain":{"data":"5ab09b52-fd81-4aee-820d-c44ef550e087","field":"data.x"}},{"name":"y","range":"height","nice":true,"domain":{"data":"5ab09b52-fd81-4aee-820d-c44ef550e087","field":"data.y"}}],"axes":[{"type":"x","scale":"x"},{"type":"y","scale":"y"}]},"value":"#gorilla_repl.vega.VegaView{:content {:width 400, :height 247.2188, :padding {:top 10, :left 50, :bottom 20, :right 10}, :data [{:name \"5ab09b52-fd81-4aee-820d-c44ef550e087\", :values ({:x \"bundesrat\", :y 1} {:x \"justizbehoerde\", :y 1} {:x \"corny-littmann\", :y 1} {:x \"bonn\", :y 2} {:x \"weizen\", :y 1} {:x \"klassik\", :y 1} {:x \"daenemark\", :y 1} {:x \"frankfurt-am-main\", :y 1} {:x \"fluechtling\", :y 4} {:x \"dresden\", :y 1})}], :marks [{:type \"rect\", :from {:data \"5ab09b52-fd81-4aee-820d-c44ef550e087\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :width {:scale \"x\", :band true, :offset -1}, :y {:scale \"y\", :field \"data.y\"}, :y2 {:scale \"y\", :value 0}}, :update {:fill {:value \"steelblue\"}, :opacity {:value 1}}, :hover {:fill {:value \"#FF29D2\"}}}}], :scales [{:name \"x\", :type \"ordinal\", :range \"width\", :domain {:data \"5ab09b52-fd81-4aee-820d-c44ef550e087\", :field \"data.x\"}} {:name \"y\", :range \"height\", :nice true, :domain {:data \"5ab09b52-fd81-4aee-820d-c44ef550e087\", :field \"data.y\"}}], :axes [{:type \"x\", :scale \"x\"} {:type \"y\", :scale \"y\"}]}}"}
+;;; {"type":"vega","content":{"width":400,"height":247.2187957763672,"padding":{"top":10,"left":50,"bottom":20,"right":10},"data":[{"name":"b3aadf48-8544-4dbd-9ab3-464ee1ef196b","values":[{"x":"bundesrat","y":1},{"x":"justizbehoerde","y":1},{"x":"corny-littmann","y":1},{"x":"bonn","y":2},{"x":"weizen","y":1},{"x":"klassik","y":1},{"x":"daenemark","y":1},{"x":"frankfurt-am-main","y":1},{"x":"fluechtling","y":4},{"x":"dresden","y":1}]}],"marks":[{"type":"rect","from":{"data":"b3aadf48-8544-4dbd-9ab3-464ee1ef196b"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"width":{"scale":"x","band":true,"offset":-1},"y":{"scale":"y","field":"data.y"},"y2":{"scale":"y","value":0}},"update":{"fill":{"value":"steelblue"},"opacity":{"value":1}},"hover":{"fill":{"value":"#FF29D2"}}}}],"scales":[{"name":"x","type":"ordinal","range":"width","domain":{"data":"b3aadf48-8544-4dbd-9ab3-464ee1ef196b","field":"data.x"}},{"name":"y","range":"height","nice":true,"domain":{"data":"b3aadf48-8544-4dbd-9ab3-464ee1ef196b","field":"data.y"}}],"axes":[{"type":"x","scale":"x"},{"type":"y","scale":"y"}]},"value":"#gorilla_repl.vega.VegaView{:content {:width 400, :height 247.2188, :padding {:top 10, :left 50, :bottom 20, :right 10}, :data [{:name \"b3aadf48-8544-4dbd-9ab3-464ee1ef196b\", :values ({:x \"bundesrat\", :y 1} {:x \"justizbehoerde\", :y 1} {:x \"corny-littmann\", :y 1} {:x \"bonn\", :y 2} {:x \"weizen\", :y 1} {:x \"klassik\", :y 1} {:x \"daenemark\", :y 1} {:x \"frankfurt-am-main\", :y 1} {:x \"fluechtling\", :y 4} {:x \"dresden\", :y 1})}], :marks [{:type \"rect\", :from {:data \"b3aadf48-8544-4dbd-9ab3-464ee1ef196b\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :width {:scale \"x\", :band true, :offset -1}, :y {:scale \"y\", :field \"data.y\"}, :y2 {:scale \"y\", :value 0}}, :update {:fill {:value \"steelblue\"}, :opacity {:value 1}}, :hover {:fill {:value \"#FF29D2\"}}}}], :scales [{:name \"x\", :type \"ordinal\", :range \"width\", :domain {:data \"b3aadf48-8544-4dbd-9ab3-464ee1ef196b\", :field \"data.x\"}} {:name \"y\", :range \"height\", :nice true, :domain {:data \"b3aadf48-8544-4dbd-9ab3-464ee1ef196b\", :field \"data.y\"}}], :axes [{:type \"x\", :scale \"x\"} {:type \"y\", :scale \"y\"}]}}"}
 ;; <=
 
 ;; @@
